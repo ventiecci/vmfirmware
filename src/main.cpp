8a398ccabac53378aef72842ef18b80dffa62b14
ferney alberto beltran molina <Ferney.Beltran@gmail.com>
2020-07-27 13:06:33 -0500

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,32 @@
 #include <servo_valve.h>
 #include <buzzer.h>
 #include <o2_valve.h>
+#include <Rtc-Ds1307.h>
 
 buzzer_ctrl   buzzer(PB_7); 
 
-Servo_valve svalve_inh(PA_1);
-Servo_valve svalve_exh(PA_15);
+DigitalIn LineAlarm(PC_3);
+DigitalIn BattAlarm(PC_2);
+
 pressure_sensor pre_sensor(PC_0);
-//o2_valve_ctrl o2_valve(PB_13);
-
-
-Blower blower1(PB_1);
-Blower blower2(PA_11);
 flow_sensor flow_sensor_inh(PB_9, PB_8);
 flow_sensor flow_sensor_exh(PB_3, PB_10);
 
-telemetry_telecontrol  cmd(USBTX, USBRX);
+o2_valve_ctrl o2_valve(PC_6);
 
+Servo_valve svalve_inh(PA_1, "SERVO INH");
+Servo_valve svalve_exh(PA_15, "SERVO EXH");
+
+Blower blower1(PB_1,"TURBINA 1");
+Blower blower2(PA_11, "tURBINA 2");
+
+
+telemetry_telecontrol  cmd(USBTX, USBRX);
+telemetry_telecontrol::Telemetry_str telemetry = {};
+
+Rtc_Ds1307 rtc(PC_9,PA_8);
+Rtc_Ds1307::Time_rtc tm = {};
+ 
 
 uint8_t VMstatus=VM_STATUS_OFF;
 
@@ -36,22 +46,25 @@ unsigned int  RR=5; // frecuencia de respiración  minuto, entre 5 y 35 por minu
 unsigned int  PIP=40;  // Maxima presión en Cm
 unsigned int  PEEP=0;   // minima presión en Cm
 unsigned int  RIR; // relación de inhalación-exhalación , para covid 1 a 2  a 1 a 3,2
+unsigned int  Fio2=21; // %  de fi02
 
-unsigned int testangle=10;
+unsigned int testangle=1;
 float tp= 60/RR; //s
 float t_inh = tp/(3); //s
 float t_exh = 2*t_inh; //s
 float t_iesleep = 0.1; //s
 
 
+unsigned int test_pulsewidth_us =1000;
+
 /*fuction*/
-void delay( int v);
+void delay( int v);   
 void off_cycle();
 void inhalation_cycle();
 void exhalation_cycle();
 void inh_exh_sleep_cycle();
 void controlON();
-
+void update_telemetry();
 /*end fuction*/
 
 void delay( int v){
@@ -59,50 +72,54 @@ void delay( int v){
     }
 
 
+void update_telemetry(){
+
+    telemetry.flow=flow_sensor_inh.read_flow()-flow_sensor_exh.read_flow();
+    telemetry.vol=flow_sensor_inh.read_flow();
+    telemetry.pre=pre_sensor.readCMH2O();
+    telemetry.time = timetest.read();
+    telemetry.BattAlarm=BattAlarm;
+    telemetry.LineAlarm=LineAlarm;
+}
 
 void controlON(){
 
-    float flow_act;
-    float  presion_act;
-
+ 
+    update_telemetry();
     switch (status_blower){
         case BLOWER_OFF:
             inhalation_cycle();
         break;
         case BLOWER_INH:
-            if( pre_sensor.max_pressure_level(presion_act)){
-//            buzzer.beep(500,0.1);
-        }
+            telemetry.PressureAlarm= pre_sensor.max_pressure_level(telemetry.pre);    
+            if(telemetry.PressureAlarm){
+                buzzer.beep(500,0.1);
+            }
         break;
         case BLOWER_EXH:
-            if( pre_sensor.min_pressure_level(presion_act)){
+            if( pre_sensor.min_pressure_level(telemetry.pre)){
                 svalve_exh.openAngle(testangle);
- //               buzzer.beep(500,0.1);
-        }
+             }
         break;
       } 
-
-    flow_act=flow_sensor_inh.read_flow()-flow_sensor_exh.read_flow();
-    presion_act=pre_sensor.readCMH2O();
-    cmd.debug_m("%0.6f \t",flow_act );
-    cmd.debug_m("%0.6f \t",presion_act);
-    cmd.sendOneData(pre_sensor.get_max_pressure());
-    cmd.debug_m("\n");
-    
-   
-    //  cmd.sendOneData( flow_act);
-    //  cmd.sendOneData( presion_act);
-
+    cmd.sendData( "VENTIECCI",telemetry);
+ 
 }
 
 void off_cycle()
 {
-    cmd.debug_m("system off\n" );
+    rtc.getTime(tm);
+ /*   cmd.debug_m("%02d:", tm.hour);
+    cmd.debug_m("%02d:", tm.min);
+    cmd.debug_m("%02d ", tm.sec);
+    cmd.debug_m("off \n" );*/
     status_blower=BLOWER_OFF;
     blower1.speed(1000);
     blower2.speed(1000);
     svalve_inh.close();
     svalve_exh.close();
+    cmd.sendData( "STANDBY",telemetry);
+
 
 }
 
@@ -110,26 +127,37 @@ void off_cycle()
 
 void inhalation_cycle(){
     status_blower=BLOWER_INH;
-    blower1.cmH2O(pre_sensor.get_max_pressure());
-    if (t_exh>1.5){ 
-        blower2.stop();
-   }
-    svalve_inh.open();
     svalve_exh.close();
     buzzer.beep(440,0.1);
+    if (Fio2<100){
+        blower1.cmH2O(pre_sensor.get_max_pressure());
+        if (t_exh>1.5){ 
+            blower2.stop();
+        }
+        svalve_inh.open();    
+    }else
+    {
+        o2_valve.openA(0.8);
   
+    }
+    
     if (VMstatus==1)
         timeout_inh.attach(&inh_exh_sleep_cycle, t_inh);   // time to off
     else
         timeout_inh.attach(&off_cycle, t_inh);   // time to off
 
-
 }
+
 void inh_exh_sleep_cycle(){
-    if (t_exh>1.5){ 
-        blower1.stop();
+    if (Fio2<100){
+        if (t_exh>1.5){ 
+            blower1.stop();
+        }
+    }else
+    {
+         o2_valve.openA(0.8); 
     }
-  
+         
     if (VMstatus==1)
         timeout_inh.attach(&exhalation_cycle, t_iesleep);   // time to off
     else
@@ -139,41 +167,21 @@ void inh_exh_sleep_cycle(){
 
 void exhalation_cycle(){
     status_blower=BLOWER_EXH;
-    blower2.cmH2O(pre_sensor.get_min_pressure());
- 
-    svalve_inh.close();
     svalve_exh.open();
+  
+    if (Fio2<100){
+      blower2.cmH2O(pre_sensor.get_min_pressure());
+      svalve_inh.close();
+    }else{
+          o2_valve.close();
+    }
+
     if (VMstatus==VM_STATUS_ON)
         timeout_exh.attach(&inhalation_cycle, t_exh);   // time to off
     else
         timeout_exh.attach(&off_cycle, t_exh);   // time to off
 }
 
-void   servo_valve_test(Servo_valve* svalve){
-    timetest.start();     // start timer counting 
-    blower1.speed(1500);
-    blower2.speed(1500);
-    bool onm=0;
-    while(VMstatus==VM_STATUS_TEST){
-       cmd.sendOneData( flow_sensor_inh.read_flow());
-        if (timetest.read_ms()>=2000) 
-        { 
-            timetest.reset(); 
-            if(onm==1) {
-                cmd.debug_m("close svalve\n" );
-                svalve->close();
-                onm=0;
-            
-            }else {
-                svalve->open();
-                cmd.debug_m("open svalve\n" );
-                onm=1;
-            }
-        }
-
-
-}
-}
 
 
 void change_resp_rate(int vRR){
@@ -202,16 +210,17 @@ int  changeParameter(){
             cmd.debug_m("el valor de T es %d\n",b);
             break;
           case TC_RESPIRATION_RATE:
-            cmd.debug_m("el valor de RR es %d\n",b);
+            cmd.debug_m("el valor de RR era %d\n",RR);
             change_resp_rate(b);
+            cmd.debug_m("el valor de RR es %d\n",RR);
             break;
           case TC_PIP:
-            cmd.debug_m("el valor de PIP es %d\n",b);
+            cmd.debug_m("el valor de PIP era %d\n",pre_sensor.get_max_pressure());
             pre_sensor.set_max_pressure(b);
             cmd.debug_m("el valor de PIP nuevo es %d\n",pre_sensor.get_max_pressure());
             break;
           case TC_PEEP:
-            cmd.debug_m("el valor de PEEP es %d\n",b);
+            cmd.debug_m("el valor de PEEP era %d\n", pre_sensor.get_min_pressure());
             pre_sensor.set_min_pressure(b);
             cmd.debug_m("el valor de PEEP es %d\n", pre_sensor.get_min_pressure());
       
@@ -227,6 +236,15 @@ int  changeParameter(){
             testangle=b;
             cmd.debug_m("el valor deL testangle es %d\n",b);
             break;
+          case TC_FiO2:
+            o2_valve.openA(((float)b)/100);
+//            o2_valve.openD(b);
+            cmd.debug_m("el valor deL fIO2 es %d\n",b);
+            break;
+          case TC_DUTTY:
+            test_pulsewidth_us =b;
+            cmd.debug_m("el valor del pulso para pwm es de  %d us\n",b);
+        break;  
           default:
             cmd.debug_m("NO SE RECONOE el comando  %c con el valor %d\n",*scmd, b);
             break;
@@ -237,39 +255,100 @@ int  changeParameter(){
 }
 
 
+void  test_blower(Blower* bw){
+    svalve_exh.close();
+    svalve_inh.open();
+ 
+    bw->speed(test_pulsewidth_us);
+
+    update_telemetry();
+    cmd.sendData(bw->getname(), telemetry);
+
+}
+
+
+void   servo_valve_test(Servo_valve* svalve){
+    blower1.speed(test_pulsewidth_us);
+    blower2.speed(test_pulsewidth_us);
+    svalve->openAngle(testangle);
+    update_telemetry();
+    cmd.sendData(svalve->getname(), telemetry);
+  
+}
+
+
+void   elect_valveO2_test(o2_valve_ctrl* ovalve){
+    blower1.stop();
+    blower2.stop();
+    ovalve->openD(test_pulsewidth_us);
+    update_telemetry();
+    cmd.sendData("TEST_O2_ELECTRO", telemetry);
+  
+}
+
 int  main()
 {
 
     VMstatus=VM_STATUS_OFF;
     flow_sensor_inh.init();  
     flow_sensor_exh.init();  
-   
+    o2_valve.close();
+    LineAlarm.mode(PullUp);
+    BattAlarm.mode(PullUp);
+ //   tm.hour=2;
+ //   tm.min=30;
+ //   tm.sec=0;
+ //   rtc.setTime(tm, true, true);
+
     cmd.debug_m("sistema iniciado test en modo debug" );
     wait(2.0);
-
+    timetest.start();
+    float time_act=timetest.read_ms();
     while(1) {
         if (cmd.newcmd_detected){  
-           cmd.newcmd_detected=false;
            changeParameter();
+           cmd.newcmd_detected=false;
         }
-        switch(VMstatus){
-        case VM_STATUS_ON:
-            controlON();
-            delay(50);
+        if (timetest.read_ms()-time_act>=200) {
+            time_act=timetest.read_ms();
+            update_telemetry();
+            switch(VMstatus){
+            case VM_STATUS_ON:
+                controlON();
+                break;
+            case VM_STATUS_ERROR:
+                break;
+            case VM_STATUS_OFF:
+                off_cycle();
             break;
-        case VM_STATUS_TEST:
-            servo_valve_test(&svalve_inh);
+            case VM_TEST_BLOWER1:
+                blower2.speed(1000);
+                test_blower(&blower1);
             break;
-        case VM_STATUS_ERROR:
+            case VM_TEST_BLOWER2:
+                blower1.speed(1000);
+                test_blower(&blower2);
             break;
-        case VM_STATUS_OFF:
-            off_cycle();
-        break;
-        default:
-            off_cycle();
-        
-        }
+            case VM_SERVO_VALVE_TEST_INH:
+                svalve_exh.close();
+                servo_valve_test(&svalve_inh);
+            break;
+            case VM_SERVO_VALVE_TEST_EXH:
+                svalve_inh.open();
+                servo_valve_test(&svalve_exh);
+            break;
 
+            case VM_ELECTRO_VALVE_O2_TEST:
+                svalve_inh.open();
+                elect_valveO2_test(&o2_valve);
+            break;
+            
+            default:
+                off_cycle();
+  
+            
+            }   
+        }
     }  
         
 }
